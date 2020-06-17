32a70758f2ec1d30f0c182728edf4b3f73809d97
MattiaBressanelli <40475194+MattiaBressanelli@users.noreply.github.com>
2020-06-17 10:59:39 +0200

    Add files via upload

---
+++ mb_advancedviewexport.rb
@@ -0,0 +1,22 @@
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module MattiaBressanelli
+  module AdvancedViewExport
+
+    unless file_loaded?(__FILE__)
+    
+      ex = SketchupExtension.new('Advanced View Export', 'mb_advancedviewexport/main')
+
+      ex.description = 'Extension to export the current view as image with style.'
+      ex.version     = '1.0.0'
+      ex.copyright   = 'Mattia Bressanelli © 2020'
+      ex.creator     = 'Mattia Bressanelli'
+
+      Sketchup.register_extension(ex, true)
+
+      file_loaded(__FILE__)
+    end
+
+  end # module AdvancedViewExport
+end # module MattiaBressanelli
