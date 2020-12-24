887407614916839af5697f90ffc14463066c67da
MattiaBressanelli <40475194+MattiaBressanelli@users.noreply.github.com>
2020-12-25 00:29:16 +0100

    Update main.rb

--- mb_advancedviewexport/main.rb
+++ mb_advancedviewexport/main.rb
@@ -12,8 +12,8 @@ module MattiaBressanelli
     $image_height = nil
     $maps_array = []
     $composite
-    $boolean_exist = false
     $ambientocclusion_exist = false
+    $boolean_exist = false
     
     # Main function
     def self.main()
@@ -41,7 +41,7 @@ module MattiaBressanelli
             UI.messagebox("Enter number between 1 and 9999")
         }
 
-        # set output folder
+        # set output directory
         dialog.add_action_callback("set_output_dir") { |action_context|
             $output_folder = set_output_dir()
             if $output_folder === nil
@@ -53,11 +53,11 @@ module MattiaBressanelli
 
         # get image name
         dialog.add_action_callback("get_image_name") { |action_context, image_name|
-            $export_name = image_name
+            $export_name = image_name.gsub(" ", "_").gsub(".", "_").gsub("/", "_").gsub("\\", "_")
         }
 
         # get maps options
-        dialog.add_action_callback("get_maps_options") { |action_context, image_width, image_height, maps_array, composite |
+        dialog.add_action_callback("get_maps_options") { |action_context, image_width, image_height, maps_array, composite|
             $image_width = image_width
             $image_height = image_height            
             $maps_array = maps_array
@@ -83,9 +83,23 @@ module MattiaBressanelli
 
     # add custom styles from folder
     def self.load_custom_styles()
-        files = Dir.glob(File.join(File.dirname(__FILE__), '/styles/*.style')).select{ |f| File.file? f }
-        files.each{ |i| 
-            Sketchup.active_model.styles.add_style(i, false)
+        # existing styles
+        existing_styles_names = []
+        Sketchup.active_model.styles.each{ |s| existing_styles_names.push(s.name) }
+
+        # new styles
+        new_styles_file_paths = Dir.glob(File.join(File.dirname(__FILE__), '/styles/*.style')).select{ |f| File.file? f }
+        new_styles_names = []
+        new_styles_file_paths.each{ |f| new_styles_names.push(f.split('/')[-1].split('.')[0]) }
+
+        count = 0
+        new_styles_file_paths.each{ |i|
+            # if new style doesn't already exist, add it
+            if( (existing_styles_names.include? new_styles_names[count]) == false)
+                Sketchup.active_model.styles.add_style(i, false)
+            end
+
+            count += 1
         }
     end
 
@@ -118,6 +132,10 @@ module MattiaBressanelli
         Sketchup.active_model.shadow_info["DisplayShadows"] = false
 
         # export
+        if composite
+            system("mkdir #{$output_folder.gsub("/", "\\")}\\maps")
+        end
+
         maps_array.each{ |m|
             if (m == "shadows_map")
                 Sketchup.active_model.shadow_info["DisplayShadows"] = true
@@ -125,7 +143,11 @@ module MattiaBressanelli
                 Sketchup.active_model.shadow_info["DisplayShadows"] = false      
             end
             style = Sketchup.active_model.styles[m]
-            style_string = $output_folder + "/" + $export_name + "_#{style.name}.png"
+            if composite
+                style_string = $output_folder + "/maps/" + $export_name + "_#{style.name}.png"
+            else
+                style_string = $output_folder + "/" + $export_name + "_#{style.name}.png"
+            end
             Sketchup.active_model.styles.selected_style = style
             Sketchup.active_model.active_view.write_image(style_string, image_width, image_height, true)
         }
@@ -142,38 +164,30 @@ module MattiaBressanelli
     # Function to composite image maps
     def self.composite_image_maps(maps_array)
 
-        background_string = $output_folder + "/background.png"
+        background_string = $output_folder + "/maps/background.png"
         system("magick convert -size #{$image_width}x#{$image_height} xc:#ffffff #{background_string}")
 
-        # adaptive blur amount
-        ambientocclusion_blur_sigma = (($image_width * $image_height) / 150000).floor()
-        shadows_blur_sigma = (($image_width * $image_height) / 600000).floor()
+        # check if ao exists
+        if (maps_array.include? "ambientocclusion_map")
+            $ambientocclusion_exist = true
+        end
+        
+        # check if bool exists
+        if (maps_array.include? "boolean_map")
+            $boolean_exist = true
+        end
 
         # edit maps before composition
-        maps_array.each{ |m| 
+        maps_array.each{ |m|
 
             style = Sketchup.active_model.styles[m]
-            style_string = $output_folder + "/" + $export_name + "_#{style.name}.png"
-
-            # blur and dissolve ambient occlusion map
-            if (m == "ambientocclusion_map")
-                $ambientocclusion_exist = true
-
-                system("cd \\")
-                # blur
-                system("magick convert #{style_string} -blur 0x#{ambientocclusion_blur_sigma} #{style_string}")
-                # set transparency to 10%
-                system("magick composite -dissolve 10 #{style_string} #{background_string} #{style_string}")
-
-            # invert boolean map
-            elsif (m == "boolean_map")
-                $boolean_exist = true
-
-                system("cd \\")
-                system("magick convert #{style_string} -negate #{style_string}")
+            style_string = $output_folder + "/maps/" + $export_name + "_#{style.name}.png"
+            
+            # adaptive blur amount
+            shadows_blur_sigma = (($image_width * $image_height) / 600000).floor()
 
             # blur and dissolve shadows map          
-            elsif (m == "shadows_map")
+            if (m == "shadows_map")
                 system("cd \\")
                 # blur
                 system("magick convert #{style_string} -blur 0x#{shadows_blur_sigma} #{style_string}")   
@@ -192,85 +206,89 @@ module MattiaBressanelli
                 # set transparency to 10%
                 system("magick composite -dissolve 10 #{style_string} #{background_string} #{style_string}")
             end
-        }    
+        }
 
         # if one of ao map and bool map doesn't exist, remove both of them from the array
+        if ($ambientocclusion_exist && $boolean_exist)
 
-        if ($ambientocclusion_exist == false || $boolean_exist == false)
-            i = 0
-            while i < (maps_array.length() - 1) do
-                if (maps_array[i] == "boolean_map")
-                    maps_array.slice(i, 1)   
-                end
-            end
+            background_string = $output_folder + "/maps/background.png"
 
-            i = 0
-            while i < (maps_array.length() - 1) do
-                if (maps_array[i] == "ambientocclusion_map")
-                    maps_array.slice(i, 1)
-                end
-            end
+            image_ao = $output_folder + "/maps/" + $export_name + "_ambientocclusion_map.png"
+            image_bool = $output_folder + "/maps/" + $export_name + "_boolean_map.png"
+            image_cmp = $output_folder + "/maps/" + $export_name + "_fixedambientocclusion_map.png"
 
-        # if both exist
-        else
-            image_ao = $output_folder + "/" + $export_name + "_ambientocclusion_map.png"
-            image_bool = $output_folder + "/" + $export_name + "_boolean_map.png"
-            image_cmp = $output_folder + "/" + $export_name + "_fixedambientocclusion_map.png"
+            # adaptive blur amount
+            ambientocclusion_blur_sigma = (($image_width * $image_height) / 150000).floor()
 
+            # blur and dissolve ambient occlusion map
+            system("cd \\")
+            # blur
+            system("magick convert #{image_ao} -blur 0x#{ambientocclusion_blur_sigma} #{image_ao}")
+            # set transparency to 10%
+            system("magick composite -dissolve 10 #{image_ao} #{background_string} #{image_ao}")
+
+            # invert boolean map
+            system("cd \\")
+            system("magick convert #{image_bool} -negate #{image_bool}")
+
+            # subtract bool from ambient occlusion
             system("cd \\")
             system("magick composite #{image_ao} #{background_string} #{image_bool} #{image_cmp}")
+
+            system("del #{$output_folder.gsub("/", "\\") + "\\maps\\" + $export_name.gsub("/", "\\") + "_boolean_map.png"}")
+            system("del #{$output_folder.gsub("/", "\\") + "\\maps\\" + $export_name.gsub("/", "\\") + "_ambientocclusion_map.png"}")
 
             maps_array.pop()
             maps_array.pop()
 
             maps_array.unshift("fixedambientocclusion_map")
+
+        else
+            if $ambientocclusion_exist
+                maps_array.pop()
+                system("del #{$output_folder.gsub("/", "\\") + "\\maps\\" + $export_name.gsub("/", "\\") + "_ambientocclusion_map.png"}")
+            end
+            if $boolean_exist
+                maps_array.pop()
+                system("del #{$output_folder.gsub("/", "\\") + "\\maps\\" + $export_name.gsub("/", "\\") + "_boolean_map.png"}")
+            end
         end
 
-        # iterate over maps array
-
+        # iterate over maps array if there are at least two elements
         i = 0
-        while i < (maps_array.length() - 1) do
-
-            image_file_src = $output_folder + "/" + $export_name + "_#{maps_array[i]}.png"
-            image_file_dst = $output_folder + "/" + $export_name + "_#{maps_array[i + 1]}.png"
-
+        if (maps_array.length() == 1)
+            image_file_src = $output_folder + "/maps/" + $export_name + "_#{maps_array[i]}.png"
             system("cd \\")
-            system("magick composite #{image_file_src} #{image_file_dst} -compose Multiply #{image_file_dst}")
+            system("move /Y #{image_file_src.gsub("/", "\\")} #{$output_folder.gsub("/", "\\")}")
+            system("rename #{$output_folder.gsub("/", "\\") + "\\" + $export_name + "_" + maps_array[i]}.png #{$export_name}.png")
+        else
+            while i < (maps_array.length() - 1) do
 
-            # rename if last element
-            if (i == maps_array.length() - 2) then
+                image_file_src = $output_folder + "/maps/" + $export_name + "_#{maps_array[i]}.png"
+                image_file_dst = $output_folder + "/maps/" + $export_name + "_#{maps_array[i + 1]}.png"
+
                 system("cd \\")
-                image_file_dst_rev = image_file_dst.gsub("/", "\\")
-                system("rename #{image_file_dst_rev} #{$export_name}.png")
-            end
+                system("magick composite #{image_file_src} #{image_file_dst} -compose Multiply #{image_file_dst}")
 
-        i += 1
+                # rename if last element
+                if (i == maps_array.length() - 2) then
+                    system("cd \\")
+                    system("move /Y #{image_file_dst.gsub("/", "\\")} #{$output_folder.gsub("/", "\\")}")
+                    system("rename #{$output_folder.gsub("/", "\\") + "\\" + $export_name + "_" + maps_array[i + 1]}.png #{$export_name}.png")
+                end
+
+                i += 1
+            end
         end
 
         # delete maps
         system("cd \\")
-        background_string_rev = background_string.gsub("/", "\\")
-        system("del #{background_string_rev}")
+        system("rmdir /Q /S -r #{$output_folder.gsub("/", "\\")}\\maps")
 
-        output_folder_rev = $output_folder.gsub("/", "\\")
-
-        image_to_delete = output_folder_rev + "\\" + $export_name + "_ambientocclusion_map.png"
-        system("cd \\")
-        system("del #{image_to_delete}")
-        
-        image_to_delete = output_folder_rev + "\\" + $export_name + "_boolean_map.png"
-        system("cd \\")
-        system("del #{image_to_delete}")
-
-        i = 0
-        while i < (maps_array.length() - 1) do
-            image_to_delete = output_folder_rev + "\\" + $export_name + "_#{maps_array[i]}.png"
-            system("cd \\")
-            system("del #{image_to_delete}")
-        i += 1
-        end
-    
+        $ambientocclusion_exist = false
+        $boolean_exist = false
     end
+
     unless file_loaded?(__FILE__)
 
       menu = UI.menu('View')
